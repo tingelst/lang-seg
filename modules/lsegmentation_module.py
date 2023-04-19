9c848dc694e9a08fa4e16c9ce5a35e949c52b6ba
Lars Tingelstad <lars.tingelstad@ntnu.no>
2023-04-19 15:56:42 +0200

    Added recipe to run lang-seg in nvidia docker container

--- modules/lsegmentation_module.py
+++ modules/lsegmentation_module.py
@@ -9,6 +9,8 @@ import torchvision.transforms as transforms
 from argparse import ArgumentParser
 
 import pytorch_lightning as pl
+import torchmetrics
+
 
 from data import get_dataset, get_available_datasets
 
@@ -209,12 +211,12 @@ class LSegmentationModule(pl.LightningModule):
         )
 
         self.num_classes = dset.num_class
-        self.train_accuracy = pl.metrics.Accuracy()
+        self.train_accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=self.num_classes)
 
         return dset
 
     def get_valset(self, dset, augment=False, **kwargs):
-        self.val_accuracy = pl.metrics.Accuracy()
+        self.val_accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=self.num_classes)
         self.val_iou = SegmentationMetric(self.num_classes)
 
         if augment == True:
